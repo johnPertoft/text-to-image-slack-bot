c09059dbc576b8bcb5e79e10f66f167e829c8b36
John Pertoft <john.pertoft@gmail.com>
2022-08-30 12:28:50 +0000

    Make sure we're on main branch.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,13 @@ set -o pipefail
 # - Maybe use cloud build instead?
 # - Automatically download the pipeline files?
 
+if [[ $(git symbolic-ref --short -q HEAD) != "main" ]]; then
+    echo "You should be on the main branch"
+    exit 1
+fi
+
+exit
+
 if [ -d "pipelines/sd-pipeline" ]; then
     gcloud auth configure-docker
     docker build -t gcr.io/embark-shared/ml2/john-stable-diffusion .
