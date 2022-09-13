a12d2884319695ffc98e86b1ae8cb336f7154285
John Pertoft <john.pertoft@gmail.com>
2022-09-13 14:52:51 +0000

    Add module for parsing query.

--- src/query.py
+++ src/query.py
@@ -1,3 +1,6 @@
+import re
+from typing import Any
+from typing import Dict
 from typing import Literal
 from typing import Optional
 
@@ -5,12 +8,6 @@ from pydantic import BaseModel
 from pydantic import Extra
 from pydantic import Field
 from pydantic import HttpUrl
-
-# TODO:
-# - Have tests for this.
-# - Just repeat some of these fields for the actual model inputs data class?
-# - Do not accept additional fields to capture spelling errors
-# - Post help message back to message thread.
 
 
 class Query(BaseModel):
@@ -27,6 +24,78 @@ class Query(BaseModel):
         extra = Extra.forbid
 
 
-def parse_query(raw: str) -> Query:
-
+class ParseQueryException(Exception):
     pass
+
+
+def parse_query(raw_query: str) -> Query:
+    # We only consider the text after the @mention. Needs to be a non greedy
+    # match so that we don't capture other slack formatting here.
+    app_mention_match = re.search(r"(<@.*?>) (.*)", raw_query)
+    if app_mention_match is None:
+        raise ParseQueryException("You have to write a text prompt too!")
+
+    # The second match group is the actual query for the app.
+    query = app_mention_match.group(2)
+
+    # The query can optionally contain a configuration to circumvent
+    # some or all of the default values.
+    query_parts = query.split("|")
+    if len(query_parts) == 1:
+        prompt = query_parts[0].strip()
+        return Query(prompt=prompt)
+    else:
+        # If "|" present in prompt part, join it back.
+        prompt = "|".join(query_parts[1:])
+        prompt = prompt.strip()
+
+        config_str = query_parts[0]
+        try:
+            config = parse_config(config_str)
+        except:  # noqa: E722
+            raise ParseQueryException("I couldn't parse that configuration!")
+
+        return Query(prompt=prompt, **config)
+
+
+def parse_config(config_str: str) -> Dict[str, Any]:
+    # We don't allow spaces to carry any significance here.
+    config_str = config_str.replace(" ", "")
+
+    # Early exit if no config supplied.
+    if config_str == "":
+        return dict()
+
+    # Slack formats urls with angled brackets like <https://...>
+    # Sometimes urls contain commas so need to take this into account
+    # when we split by comma to separate each config item so we first
+    # find these urls (should only be one).
+    url_matches = re.finditer(r"<.*?>", config_str)
+    url_spans = [url_match.span() for url_match in url_matches]
+
+    # The valid commas to split at are the ones not within the url match
+    # spans.
+    all_comma_indices = [m.span()[0] for m in re.finditer(",", config_str)]
+    split_indices = [i for i in all_comma_indices if not any(s1 <= i < s2 for s1, s2 in url_spans)]
+    prev_idx = 0
+    split_spans = []
+    for split_idx in split_indices:
+        split_spans.append((prev_idx, split_idx))
+        prev_idx = split_idx + 1
+    split_spans.append((prev_idx, None))  # type: ignore
+
+    # Split into each config item and create a config dict.
+    config_records = [config_str[i:j] for i, j in split_spans]
+    config = dict([c.split("=", 1) for c in config_records])
+
+    def maybe_remove_slack_formatting(x: str) -> str:
+        m = re.search(r"<(.*?)>", x)
+        if m is not None:
+            return m.group(1)
+        else:
+            return x
+
+    # Remove any Slack formatting brackets
+    config = {k: maybe_remove_slack_formatting(v) for k, v in config.items()}
+
+    return config
--- src/query_test.py
+++ src/query_test.py
@@ -1,3 +1,7 @@
+import pytest
+from pydantic import ValidationError
+
+from .query import ParseQueryException
 from .query import parse_query
 
 # TODO:
@@ -6,23 +10,86 @@ from .query import parse_query
 # - Test for case insensitivity etc
 # - Pytest can't access our dependencies
 #   Maybe use pyenv or something instead?
+# - Create some text fixture for creating the slack formatting
+# - Add assertions on default values.
 
 
 def test_just_prompt_str():
-    raw = "@burgerman a red apple"
+    raw = "<@burgerman> a red apple"
     q = parse_query(raw)
     assert q.prompt == "a red apple"
 
 
-def test_prompt_and_config():
-    raw = "@burgerman seed=123, format=tall | a red apple"
+def test_simple_config():
+    raw = "<@burgerman> seed=123, format=tall | a red apple"
+    q = parse_query(raw)
+    assert q.prompt == "a red apple"
+    assert q.seed == 123
+    assert q.format == "tall"
+
+
+def test_config_with_uri():
+    raw = "<@burgerman> img_uri=<https://test.url/img.png> | a red apple"
+    q = parse_query(raw)
+    assert q.prompt == "a red apple"
+    assert q.img_uri == "https://test.url/img.png"
+
+
+def test_config_with_uri_with_commas():
+    raw = "<@burgerman> img_uri=<https://test.url/img.png?abc=1,2,3> | a red apple"
+    q = parse_query(raw)
+    assert q.prompt == "a red apple"
+    assert q.img_uri == "https://test.url/img.png?abc=1,2,3"
+
+
+def test_config_with_uri_last():
+    raw = "<@burgerman> seed=123, img_uri=<https://test.url/img.png?abc=1,2,3> | a red apple"
+    q = parse_query(raw)
+    assert q.prompt == "a red apple"
+    assert q.img_uri == "https://test.url/img.png?abc=1,2,3"
+    assert q.seed == 123
+
+
+def test_config_with_extra_spaces():
+    raw = "<@burgerman> seed = 123 , img_uri= <https://test.url/img.png?abc=1,2,3> | a red apple"
+    q = parse_query(raw)
+    assert q.prompt == "a red apple"
+    assert q.img_uri == "https://test.url/img.png?abc=1,2,3"
+    assert q.seed == 123
+
+
+def test_empty_config():
+    raw = "<@burgerman> | a red apple"
     q = parse_query(raw)
     assert q.prompt == "a red apple"
 
 
-def test_malformed():
-    pass
+def test_malformed_no_mention():
+    raw = "a red apple"
+    with pytest.raises(ParseQueryException):
+        parse_query(raw)
+
+
+def test_malformed_config():
+    raw = "<@burgerman> ,,,,, | a red apple"
+    with pytest.raises(ParseQueryException):
+        parse_query(raw)
 
 
 def test_undefined_config_item():
-    pass
+    raw = "<@burgerman> undefined_option=123 | a red apple"
+    with pytest.raises(ValidationError):
+        parse_query(raw)
+
+
+def test_invalid_config_value():
+    raw = "<@burgerman> seed=hmmm | a red apple"
+    with pytest.raises(ValidationError):
+        parse_query(raw)
+
+
+def test_misspelled_img_uri():
+    # Note: img_urL not img_urI here.
+    raw = "<@burgerman> img_url=<https://test.url/img.png?abc=1,2,3> | a red apple"
+    with pytest.raises(ValidationError):
+        parse_query(raw)
