7785d7435c0816b3e8f4e4a595763ed0e6278a16
John Pertoft <john.pertoft@gmail.com>
2022-12-26 15:12:20 +0000

    Update query parsing method.

--- src/app.py
+++ src/app.py
@@ -14,8 +14,8 @@ from slack_bolt.async_app import AsyncApp
 
 from .constants import GCP_SLACK_BOT_TOKEN_SECRET_NAME
 from .constants import GCP_SLACK_SIGNING_SECRET_NAME
-from .constants import SLACK_APP_NAME
 from .pipeline import CombinedPipelineInputs
+from .query import USAGE_STR
 from .query import ParseQueryException
 from .query import Query
 from .query import parse_query
@@ -27,20 +27,6 @@ from .worker import WorkerProcess
 
 logging.basicConfig(level=logging.INFO)
 
-USAGE_STR = f"""
-Usage examples
-@{SLACK_APP_NAME} A horse in space
-@{SLACK_APP_NAME} seed=123, format=wide | A horse in space
-@{SLACK_APP_NAME} img_url=https://url.to.my/image.png | A horse in space
-
-Config options
-seed: int
-num_inference_steps: int: [1, 100], default 50
-guidance_scale: float: [1.0, 15.0], default 7.5
-img_url: HttpUrl, use this image as starting image in img2img
-strength: float: [0.0, 1.0], default 0.8, only used for img2img
-format: Literal["square", "tall", "wide"] default square, not used in img2img
-"""
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
@@ -70,26 +56,23 @@ worker_process.start()
 
 
 async def prepare_pipeline_inputs(query: Query) -> CombinedPipelineInputs:
-    if query.img_url is not None:
+    query_dict = query.dict()
+
+    # Download image if necessary.
+    if query_dict["img_url"] is not None:
         logging.info(f"Downloading {query.img_url}")
         img = await download_img(
             query.img_url, slack_token=get_secret(GCP_SLACK_BOT_TOKEN_SECRET_NAME)
         )
+        query_dict["init_img"] = img
     else:
-        img = None
+        query_dict["init_img"] = None
 
-    return CombinedPipelineInputs(
-        prompt=query.prompt,
-        negative_prompt=query.negative_prompt,
-        seed=query.seed or random.randint(0, 10000),
-        guidance_scale=query.guidance_scale,
-        num_inference_steps=query.num_inference_steps,
-        img_url=query.img_url,
-        init_img=img,
-        format=query.format,
-        nsfw_allowed=query.nsfw_allowed,
-        tshirt_mode=query.tshirt_mode,
-    )
+    # Always define a seed for the model inputs.
+    if query_dict["seed"] is None:
+        query_dict["seed"] = random.randint(0, 10000)
+
+    return CombinedPipelineInputs(**query_dict)
 
 
 @app.middleware
@@ -111,8 +94,6 @@ async def app_mention(body: Dict[str, Any], say: Say):
         query = parse_query(raw_event_text)
     except ParseQueryException as e:
         return await say_in_thread(f"Oops! {e}\n\n{USAGE_STR}")
-    except ValidationError:
-        return await say_in_thread(f"Oops! I couldn't validate those inputs!\n\n{USAGE_STR}")
 
     try:
         pipeline_inputs = await prepare_pipeline_inputs(query)
--- src/query.py
+++ src/query.py
@@ -1,6 +1,6 @@
+import argparse
 import re
-from typing import Any
-from typing import Dict
+import shlex
 from typing import Literal
 from typing import Optional
 
@@ -8,6 +8,13 @@ from pydantic import BaseModel
 from pydantic import Extra
 from pydantic import Field
 from pydantic import HttpUrl
+from pydantic import ValidationError
+
+from .constants import SLACK_APP_NAME
+
+
+class ParseQueryException(Exception):
+    pass
 
 
 class Query(BaseModel):
@@ -19,15 +26,33 @@ class Query(BaseModel):
     guidance_scale: float = Field(default=7.5, ge=1.0, le=15.0)
     strength: float = Field(default=0.8, ge=0.0, le=1.0)
     format: Literal["square", "tall", "wide"] = "square"
-    nsfw_allowed: bool = False
     tshirt_mode: bool = False
 
     class Config:
         extra = Extra.forbid
 
 
-class ParseQueryException(Exception):
-    pass
+assert sum(f.required for f in Query.__fields__.values()), "Just one required arg allowed"
+
+# Create an argument parser from the Query model.
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("prompt", nargs="+")
+for argname, field in Query.__fields__.items():
+    if not field.required:
+        if field.type_ == bool:
+            PARSER.add_argument(
+                f"--{argname}", action=argparse.BooleanOptionalAction, default=False
+            )
+        else:
+            PARSER.add_argument(f"--{argname}")
+
+# Create a usage string.
+USAGE_STR = PARSER.format_help()
+usage_str_lines = re.split(r"\n+", USAGE_STR)
+usage_str_lines = [line for line in usage_str_lines if "show this help message" not in line]
+USAGE_STR = "\n".join(usage_str_lines)
+USAGE_STR = USAGE_STR.replace("[-h]", "")
+USAGE_STR = USAGE_STR.replace("app.py", f"@{SLACK_APP_NAME}")
 
 
 def parse_query(raw_query: str) -> Query:
@@ -40,91 +65,28 @@ def parse_query(raw_query: str) -> Query:
     # The second match group is the actual query for the app.
     query = app_mention_match.group(2)
 
-    # The query can optionally contain a configuration to circumvent
-    # some or all of the default values.
-    query_parts = query.split("|")
-    if len(query_parts) == 1:
-        prompt = query_parts[0].strip()
-        return Query(prompt=prompt)
+    # Parse query.
+    try:
+        args = PARSER.parse_args(shlex.split(query))
+        config = vars(args)
+        config = {k: v for k, v in config.items() if v is not None}
+    except:  # noqa: E722
+        raise ParseQueryException("I couldn't parse that configuration!")
+
+    # Special handling for some arguments.
+    config["prompt"] = " ".join(config["prompt"])
+    if "img_url" in config:
+        config["img_url"] = remove_slack_link_formatting(config["img_url"])
+
+    try:
+        return Query(**config)
+    except ValidationError:
+        raise ParseQueryException("I couldn't validate those inputs!")
+
+
+def remove_slack_link_formatting(x: str) -> str:
+    m = re.search(r"<(.*?)>", x)
+    if m is not None:
+        return m.group(1)
     else:
-        # If "|" present in prompt part, join it back.
-        prompt = "|".join(query_parts[1:])
-        prompt = prompt.strip()
-
-        config_str = query_parts[0]
-        try:
-            config = parse_config(config_str)
-        except:  # noqa: E722
-            raise ParseQueryException("I couldn't parse that configuration!")
-
-        return Query(prompt=prompt, **config)
-
-
-def parse_config(config_str: str) -> Dict[str, Any]:
-    config_str = remove_insignificant_spaces(config_str)
-
-    # Early exit if no config supplied.
-    if config_str == "":
-        return dict()
-
-    quotes_matches = re.finditer(r"\".*\"", config_str)
-    quotes_spans = [q.span() for q in quotes_matches]
-
-    # Slack formats urls with angled brackets like <https://...>
-    # Sometimes urls contain commas so need to take this into account
-    # when we split by comma to separate each config item so we first
-    # find these urls (should only be one).
-    url_matches = re.finditer(r"<.*?>", config_str)
-    url_spans = [url_match.span() for url_match in url_matches]
-
-    # The valid commas to split at are the ones not within the url match
-    # spans and not within quotes.
-    non_split_spans = set(quotes_spans + url_spans)
-    all_comma_indices = [m.span()[0] for m in re.finditer(",", config_str)]
-    split_indices = [
-        i for i in all_comma_indices if not any(s1 <= i < s2 for s1, s2 in non_split_spans)
-    ]
-    prev_idx = 0
-    split_spans = []
-    for split_idx in split_indices:
-        split_spans.append((prev_idx, split_idx))
-        prev_idx = split_idx + 1
-    split_spans.append((prev_idx, None))  # type: ignore
-
-    # Split into each config item and create a config dict.
-    config_records = [config_str[i:j] for i, j in split_spans]
-    config = dict([c.split("=", 1) for c in config_records])
-
-    def maybe_remove_slack_formatting(x: str) -> str:
-        m = re.search(r"<(.*?)>", x)
-        if m is not None:
-            return m.group(1)
-        else:
-            return x
-
-    # Remove any Slack formatting brackets
-    config = {k: maybe_remove_slack_formatting(v) for k, v in config.items()}
-
-    def maybe_remove_quotes(x: str) -> str:
-        m = re.search(r"\"(.*?)\"", x)
-        if m is not None:
-            return m.group(1)
-        else:
-            return x
-
-    # Remove quotes from string arguments.
-    config = {k: maybe_remove_quotes(v) for k, v in config.items()}
-
-    return config
-
-
-def remove_insignificant_spaces(x: str) -> str:
-    pattern = re.compile(r'"[^"]*"|(\s+)')
-
-    def replacement(m):
-        if m.group(1):
-            return ""
-        else:
-            return m.group(0)
-
-    return pattern.sub(replacement, x)
+        return x
